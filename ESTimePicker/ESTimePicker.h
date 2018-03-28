74a11fbcfbdcad39fa2598efcb8302de8e668969
Philipp Maluta <fil.the.ensoreus@gmail.com>
2018-03-28 14:16:46 +0300

    fixed header

--- ESTimePicker/ESTimePicker.h
+++ ESTimePicker/ESTimePicker.h
@@ -10,10 +10,10 @@
 #import <ESTimePicker/ESTimePicker_private.h>
 
 //! Project version number for ESTimePicker.
-FOUNDATION_EXPORT double ESTimePickerVersionNumber = 0.5.2;
+FOUNDATION_EXPORT double ESTimePickerVersionNumber = 0.5;
 
 //! Project version string for ESTimePicker.
-FOUNDATION_EXPORT const unsigned char ESTimePickerVersionString[] = "0.5.2";
+FOUNDATION_EXPORT const unsigned char ESTimePickerVersionString[] = "0.5";
 
 // In this header, you should import all the public headers of your framework using statements like #import <ESTimePicker/PublicHeader.h>
 
